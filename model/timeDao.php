e748c83cec7f423ed469da24a8f2e44bd03a070d
Marcos Lima <marcosanthony21@hotmail.com>
2020-11-26 16:08:04 -0300

    Salvando time
    - salva e altera time

--- model/timeDao.php
+++ model/timeDao.php
@@ -8,12 +8,45 @@
             $this->conexao = $conexao->conectar();
         }
 
+        // inserindo e  alterar
         public function salvar($time) {
-    
+            if($time->__get('id') == 0) {
+                $sql = 'INSERT INTO time (nome, cidade, qntVitoria, anoFundacao, idTreinador) VALUES (:nome, :cidade, :qntVitoria, :anoFundacao, :idTreinador)';
+            } else {
+                $sql = 'UPDATE time SET nome = :nome, cidade = :cidade, qntVitoria = :qntVitoria, anoFundacao = :anoFundacao, idTreinador = :idTreinador WHERE id = :id';
+            }
+
+            $stmt = $this->conexao->prepare($sql);
+            $stmt->bindValue(':nome', $time->__get('nome'));
+            $stmt->bindValue(':cidade', $time->__get('cidade'));
+            $stmt->bindValue(':qntVitoria', $time->__get('qntVitoria'));
+            $stmt->bindValue(':anoFundacao', $time->__get('anoFundacao'));
+
+            $treinador = $time->__get('treinador');
+
+            $stmt->bindValue(':idTreinador', $treinador->__get('id'));
+            
+            if($time->__get('id') != 0) {
+                $stmt->bindValue(':id', $time->__get('id'));
+            }
+            
+            $stmt->execute();       
+
+            $time->__set('id', $this->conexao->lastInsertId());
+            $this->addAtletas($time);
+
+            return $time;
         }
 
-        private function addAtleta($time) {
-    
+        // gravar os dados dentro da tabela atletaTime.
+        private function addAtletas(Time $time) {
+            foreach($time->__get('atletas') as $atleta => $objeto) {
+                $sql = "INSERT INTO atletaTime(idTime, idAtleta) VALUES (:idTime, :idAtleta)";
+                $stmt = $this->conexao->prepare($sql);
+                $stmt->bindValue(':idTime', $time->__get('id'));
+                $stmt->bindValue(':idAtleta', $objeto->id);
+                $stmt->execute();
+            }
         }
 
         public function pesquisarId($id) {
--- test/timeTest.php
+++ test/timeTest.php
@@ -27,4 +27,53 @@
     print_r($time);
     echo "</pre>";
 
+
+    // iniciando teste de Salvar
+    // echo "<h3>Testando Salvar</h3>";
+
+    // $atletaDao = new AtletaDao($conexao);
+    // $treinadorDao =  new TreinadorDao($conexao);
+
+    // $atletas =  array();
+
+    // $atletas[] = $atletaDao->pesquisarId(7);
+    // $atletas[] = $atletaDao->pesquisarId(8);
+
+    // $treinador = $treinadorDao->pesquisarId(4);
+
+
+    // $timeSalvar =  new Time('Ajax', 'Campinas', $treinador, $atletas);
+    // $timeSalvar->__set('qntVitoria', 25);
+    // $timeSalvar->__set('anoFundacao', 1910);
+
+
+    // echo "<pre>";
+    // print_r($timeDao->salvar($timeSalvar));
+    // echo "</pre>";
+            
+    
+    // // Salvar - Teste alterar
+    // echo "<h3>Time antes de alterar</h3>";
+    // $timeUp = $timeDao->pesquisarId(4);
+    // echo "<pre>";
+    // print_r($timeUp);
+    // echo "</pre>";
+    
+    // // // Define um novos valores para os atributos
+    // $timeUp->__set('nome', 'Time UPUP');
+    // $timeUp->__set('cidade', 'Itapaci');
+    // $timeUp->__set('qntVitoria', 25);
+    // $timeUp->__set('anoFundacao', 1969);
+
+
+    // // Executa o método que fará a alteração dos dados do time no BD
+    // $timeDao->salvar($timeUp);
+
+    // // Pesquisa e impressão dos dados novamente, para exibir a alteração.
+    // echo "<h3>Time apos alteracoes </h3>";
+    // $timeUp = $timeDao->pesquisarId(4);
+    // echo "<pre>";
+    // print_r($timeUp);
+    // echo "</pre>"; 
+
 ?>
